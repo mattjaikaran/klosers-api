966c76b131630fcaa77f8633a27b6bca73cbabc9
Matt Jaikaran <mattjaikaran@gmail.com>
2023-09-29 16:07:19 -0400

    updated static

--- api/settings.py
+++ api/settings.py
@@ -242,11 +242,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "static/"
-# STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# STATIC
+
+ADMIN_URL = "admin/"
+ADMINS = [("mattjaikaran@gmail.com")]
+MANAGERS = ADMINS
