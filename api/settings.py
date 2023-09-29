f66e6be2885eb55d8adc6297cb0f9f18382e9bb5
Matt Jaikaran <mattjaikaran@gmail.com>
2023-09-29 16:09:13 -0400

    updated static2

--- api/settings.py
+++ api/settings.py
@@ -244,7 +244,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
