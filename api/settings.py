8c2f2b3bd2a6ecf2e9bb7a1dea99ef4b4e8a25ef
Matt Jaikaran <mattjaikaran@gmail.com>
2023-10-01 14:48:42 -0400

    revert staticfiles:

--- api/settings.py
+++ api/settings.py
@@ -266,8 +266,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 WHITENOISE_USE_FINDERS = True
 WHITENOISE_MANIFEST_STRICT = False
 WHITENOISE_ALLOW_ALL_ORIGINS = True
