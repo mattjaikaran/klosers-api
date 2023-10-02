e3006a5cc114c4e1cc96112047c6350b9df13389
Matt Jaikaran <mattjaikaran@gmail.com>
2023-10-01 23:17:36 -0400

    updated railway route

--- api/settings.py
+++ api/settings.py
@@ -56,7 +56,7 @@ CORS_ALLOWED_ORIGINS = [
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://web-production-7509.up.railway.app",
+    "https://klosers.up.railway.app/",
 ]
 
 # Application definition
