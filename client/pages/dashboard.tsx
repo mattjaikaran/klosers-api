938ca1f0b6229b121769046f88a4a37f91e5bf35
Matt Jaikaran <mattjaikaran@gmail.com>
2023-10-01 23:20:28 -0400

    trigger build

--- client/pages/dashboard.tsx
+++ client/pages/dashboard.tsx
@@ -1,11 +1,8 @@
 import Head from 'next/head';
 import AuthLayout from '@/layouts/AuthLayout';
 import Container from 'react-bootstrap/Container';
-// import Button from 'react-bootstrap/Button';
-// import { signIn, signOut, useSession } from 'next-auth/react';
 
 export default function Dashboard() {
-  // const { data: session } = useSession();
   return (
     <div>
       <Head>
@@ -17,9 +14,6 @@ export default function Dashboard() {
       <AuthLayout>
         <Container>
           <h2>Dashboard</h2>
-          {/* <p>signed in as {session?.user?.email}</p>
-          <Button onClick={() => signOut()}>Sign out</Button>
-          {session?.accessToken && <p>User has access token</p>} */}
         </Container>
       </AuthLayout>
     </div>
